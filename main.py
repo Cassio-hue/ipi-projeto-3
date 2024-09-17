5e9951f8a232066ab2ab427c9b27dce17036b4e5
“Cassio-hue” <cassiocvtb2003@gmail.com>
2024-09-17 19:01:31 -0300

    tentando outros parêmetros para detecção de cabeças

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-ARQUIVO_VIDEO = './videos/video_6.mp4'
 
 def calcula_HSI_pele(media):
 
@@ -49,28 +49,37 @@ def detecta_cabeca(imagem_cinza, x, y, w, h):
     proporcao_cabeca_corpo = 65 / 1000
     raio_estimado = int(h * proporcao_cabeca_corpo)
 
+    # Ajuste dos parâmetros de HoughCircles
     circulos_detectados = cv2.HoughCircles(imagem_cinza, cv2.HOUGH_GRADIENT, dp=1.2, minDist=raio_estimado,
-                                           param1=50, param2=30, minRadius=raio_estimado-5, maxRadius=raio_estimado+5)
+                                           param1=80, param2=30, minRadius=raio_estimado-10, maxRadius=raio_estimado+10)
 
     if circulos_detectados is not None:
         circulos_detectados = np.round(circulos_detectados[0, :]).astype("int")
         for (cx, cy, r) in circulos_detectados:
             cv2.circle(imagem_cinza, (cx, cy), r, (0, 255, 0), 2)
             cv2.circle(imagem_cinza, (cx, cy), 2, (0, 0, 255), 3)
+    
+    # Para visualizar o resultado da detecção
+    # plt.figure(figsize=(6, 6))
+    # plt.imshow(imagem_cinza, cmap='gray')
+    # plt.title('Detecção de Cabeça')
+    # plt.axis('off')
+    # plt.show()
 
-# Open the video file or capture from camera
+
+ARQUIVO_VIDEO = './videos/video_6.mp4'
 cap = cv2.VideoCapture(ARQUIVO_VIDEO)
 
-# Read the first two frames
+# Leitura dos frames
 _, frame1 = cap.read()
 _, frame2 = cap.read()
 
 while cap.isOpened():
-    # Compute the absolute difference between the two frames
-    diff = cv2.absdiff(frame1, frame2)
+    # Calcula a diferença entre os frames
+    diferenca = cv2.absdiff(frame1, frame2)
     
-    # Convert the result to grayscale
-    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    # Converte para a escala de cinza
+    gray = cv2.cvtColor(diferenca, cv2.COLOR_BGR2GRAY)
     hsv = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
     
     objetos = detecta_objetos_movimento(imagem_cinza= gray)
@@ -79,7 +88,7 @@ while cap.isOpened():
 
     area_pele = detecta_areas_pele(imagem_hsv=hsv, m=media_luminosidade)
 
-    # Draw the objetos on the original frame
+    # Detectar objetos em movimento
     for objeto in objetos:
         if cv2.contourArea(objeto) < 1000:  # Ignore small objetos
             continue
@@ -88,32 +97,31 @@ while cap.isOpened():
         # Cálculo de α (razão entre largura e altura)
         alpha = w / float(h)
 
-        #desativei a detecção por proporção do ojeto => funciona para alguns frames
-        #pro vídeo 5 funciona melhorsinho
+        # Desativei a detecção por proporção do ojeto => funciona para alguns frames
+        # Pro vídeo 5 funciona melhorsinho
         if True:#0.231 <= alpha <= 0.357:
             area_objeto_movimento = area_pele[y:y+h, x:x+w]
 
             pixels_pele = cv2.countNonZero(area_objeto_movimento)
             total_pixels = area_objeto_movimento.size
             
-            #desativei a detecção de pele => praticamente não funciona 
+            # Desativei a detecção de pele => praticamente não funciona 
             if True: #pixels_pele / total_pixels > 0.1:
 
                 # Aplicar a Transformada de Hough na região de pele
-                detecta_cabeca(gray[y:y+h, x:x+w], x, y, w, h) #deveria desenhar um círculo das cabeças detectadas
+                detecta_cabeca(gray[y:y+h, x:x+w], x, y, w, h) # Deveria desenhar um círculo das cabeças detectadas
                 cv2.rectangle(frame1, (x, y), (w + x, h + y), (0, 255, 0), 2)
     
-    # Display the result
+    # Plota o resultado
     cv2.imshow("Pedestrian Detection", frame1)
 
-    # Update the frames for the next iteration
+    # Faz a atualização dos frames do vídeo
     frame1 = frame2
     _, frame2 = cap.read()
 
-    # Break the loop with the 'q' key
+    # Aperte 'q' para sair
     if cv2.waitKey(30) & 0xFF == ord('q'):
         break
 
-# Release resources
 cap.release()
 cv2.destroyAllWindows()
